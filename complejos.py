ce21e5da52964798727fa42e9289f0b2986c91bb
ronis97 <59786481+ronis97@users.noreply.github.com>
2020-01-15 11:00:08 -0500

    Create complejos.py

---
+++ complejos.py
@@ -0,0 +1,49 @@
+from sys import stdin
+
+def suma(num1,num2):
+    #num1 y num2 deben ser arreglos
+    c = num1[0] + num2[0]
+    d = num1[1] + num2[1]
+    tot = [c,d]
+    return tot
+
+def resta(num1,num2):
+    
+    c = num1[0] - num2[0]
+    d = num1[1] - num2[1]
+    tot = [c,d]
+    return tot
+
+def multiplicar(num1,num2):
+    c = num1[0]*num2[0] - num1[1]*num2[1]
+    d = num1[0]*num2[1] + num1[1]*num2[0]
+    tot = [c,d]
+    return tot
+
+def dividir(num1,num2):
+    nume1 = num1[0] * num2[0] + num1[1]* num2[1]
+    deno = num1[0]^2 + num2[1]^2
+    num2 = num1[1]*num2[0] - num1[0]*num2[1]
+def mostrar(n):
+    """a es la parte real y b es la parte imaginaria"""
+    a = n[0]
+    b = n[1]
+    
+    if b >= 0:
+        
+        print(a,'+',str(b)+'i')
+    elif b == 0:
+        print(a)
+        
+    else:
+        print(a,'-',str(b)+'i')
+
+def main():
+    n1 = [2,3]
+    n2 = [1,0]
+    n3 = suma(n1,n2)
+    mostrar(n1)
+    mostrar(n2)
+    mostrar(n3)
+
+main()
