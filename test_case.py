6f790edb3d7eb0ae9e95ad06ef306833bc61bf65
ronis97 <59786481+ronis97@users.noreply.github.com>
2020-01-25 16:45:33 -0500

    Update test_case.py

--- test_case.py
+++ test_case.py
@@ -8,16 +8,22 @@ def test_producto():
 def test_division():
     #print(c.dividir([-7,3],[1,1]))
     assert c.dividir([-7,3],[1,1]) == [-2.0,5.0], 'Debe ser -2 +5i'
-def conjugado():
+def test_conjugado():
     assert c.conjugado([-4,5]) == [-4,-5], 'Debe ser -4-5i'
-def modulo():
-    assert c.modulo([1,1]) == 1.4142, 'Debe ser 1.4142'
-def fase():
-    assert c.fase_complejo
-
+def test_modulo():
+    assert c.modulo([1,1]) == 1.4142135623730951, 'Debe ser 1.4142'
+def test_fase():
+    assert c.fase_complejo([0,1]) == 1.5707963267948966 ,'Debe ser pi/2'
+def test_pasar_a_polar():
+    assert c.pasar_a_polar([1,1]) == [1.4142135623730951, 0.7853981633974483], 'Debe ser sqrt(2),pi/4'
+ 
 
 if __name__ == "__main__":
     test_suma()
     test_producto()
     test_division()
+    test_conjugado()
+    test_modulo()
+    test_fase()
+    test_pasar_a_polar()
     print("Prueba exitosa")
